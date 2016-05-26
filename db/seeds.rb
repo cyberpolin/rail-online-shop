83c3e1da0615e13520187879eddb7102e06943ca
Buck <buck9063@gmail.com>
2016-05-26 12:35:56 -0700

    Seed users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ require 'faker'
 CategoryProduct.delete_all
 Category.delete_all
 Product.delete_all
+User.delete_all
 
 10.times do
 	Category.create(name: Faker::Commerce.department)
@@ -20,5 +21,10 @@ end
 20.times do 
 	Product.all.sample.categories << Category.all.sample
 end
+
+User.create(username: 'tim', email: 'tim@tim.com', password: 'timtim', utype: 'admin')
+User.create(username: 'tom', email: 'tom@tom.com', password: 'tomtom', utype: 'user')
+
+
 	
 
