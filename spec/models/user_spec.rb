7a06be108b0a045bfd3d91379608a3293bb35bac
Buck & Carlos Gonzalez <pair+buckmelton.cyberpolin@devbootcamp.com>
2016-05-26 18:16:56 -0700

    Add user changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {User.new(username: "usuario", email:'cyberpolin@gmail',password: '123' );}
-  let(:bad_user) {User.new(username: "usuario", email:'cyberpolin@gmail' );}
+  let(:cool_user) {User.new(username: "usuario", email:'cyberpolin@gmail.com',password: '123' )}
+  let(:bad_user) {User.new(username: "usuario", email:'cyberpolin@gmail.com' )}
 
   context 'save' do
     it 'returns true' do
-       expect{user.save}.to change(User, :count).by(1)
+       expect{cool_user.save}.to change(User, :count).by(1)
     end
 
     it "returns false" do
       expect{bad_user.save}.to change(User, :count)
     end
   end
+
 end
