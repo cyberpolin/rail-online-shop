b2dd92b43076ee762028f862daf846d04cd3ad19
Buck & Carlos Gonzalez <pair+buckmelton.cyberpolin@devbootcamp.com>
2016-05-28 19:28:54 -0700

    Half checkput

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,2 +1,35 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(document).ready(function(){
+
+console.log('inside jquery')
+
+  $('#place_order').on('submit', function(event) {
+
+    event.preventDefault();
+
+    console.log('clicked place order:')
+    console.log(this)
+
+    var cart = localStorage.getItem('cart');
+    var the_data = JSON.parse(cart);
+
+    console.log('cart:')
+    console.log(cart)
+
+
+
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'POST',
+      data: the_data
+    }).
+      done(function(response){
+        console.log(response)
+    });
+
+  });
+
+})
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,29 @@
 class OrdersController < ApplicationController
+  def new
+
+  end
+
+  def create
+    items = params[:items]
+
+    product = Product.find(items['0']['id'])
+    order = Order.new(user_id: current_user.id)
+
+    if order.save
+      # items.each |k, v|{
+
+      # }
+      order_products = OrdersProduct.new(order: order, product_id: items['0']['id'], unit_price: items['0']['price'], qty: product.qty_in_stock)
+
+       if order_products.save
+         render plain: "{success: true}"
+       else
+         render plain: "{success: false}"
+       end
+
+    else
+      render plain: "{success: false}"
+    end
+
+  end
 end
